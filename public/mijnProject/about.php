10ceabf7f1a122089d86c483e1092a2e56d611dd
jayleevanligten <150048@csgaugustinus.nl>
2020-06-21 11:50:40 +0000

    Changes

--- public/mijnProject/about.php
+++ public/mijnProject/about.php
@@ -1,1 +1,33 @@
-Kun je deze pagina zelf maken in de stijl van de website?
+<?php
+$paginaTitel = 'About';
+$paginaType = 'openbaar';
+require('php/header.php');
+?>
+
+<h1>About CoroNews</h1>
+<p2>Hi! Ik ben Jaylee, de maker van deze website.</p2>
+<br>
+Welkom op de CoroNews site!
+</br>
+<br>
+Dit is een website die gemaakt is voor de mensen die informatie zoeken over het Coronavirus. Er wordt veel over gesproken in de media, maar er is veel onduidelijkheid. 
+Niet alles wat er in het nieuws gezegd wordt, kun je geloven. Daarom was het mijn idee om deze website te maken, zodat misinformatie zo veel mogelijk voorkomen wordt. 
+</br>
+<br>
+Op deze site kunnen mensen updates vinden zoals: nieuwsartikelen met nieuwe informatie, huidig geldende regels in Nederland en zelfs de hoeveelheid besmetingen in hun eigen provincie.
+Men kan de reacties lezen van anderen op mijn posts, zodat ze kunnen lezen hoe andere mensen over de situatie denken. Dit is mogelijk zonder een account, 
+maar met een account kan men zelf reageren op mijn posts. Hiermee kunnen mensen hun mening uiten en delen met anderen.
+Ik hoop dat ik op deze manier een deel van de stress kan wegnemen die deze situatie heeft veroorzaakt.
+</br>
+<br>
+Veel plezier met het gebruik van mijn website!
+</br>
+<br>
+Jaylee
+</br>
+<br>
+</br>
+
+<?php
+require('php/footer.php');
+?>

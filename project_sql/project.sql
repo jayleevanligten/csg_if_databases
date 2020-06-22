6a042770fec9d16ee2a673d3ceb22b2c9f837b24
jayleevanligten <150048@csgaugustinus.nl>
2020-06-22 21:13:19 +0000

    Changes

--- project_sql/project.sql
+++ project_sql/project.sql
@@ -80,7 +80,7 @@ CREATE TABLE `reacties` (
   `account` varchar(32) NOT NULL,
   `reactie` varchar(32) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -102,4 +102,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-22 20:28:05
+-- Dump completed on 2020-06-22 20:46:14
